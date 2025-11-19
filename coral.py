6cb7403b81023192837a4af96945b9caa243b493
Bryan Stillwell <bryan.stillwell@clyso.com>
2025-11-19 23:30:33 +0000

    Gather cluster details and store it in cluster_info

--- coral.py
+++ coral.py
@@ -1,7 +1,120 @@
 #!/usr/bin/env python3
 
+import json
+import rados
+from pprint import pprint
+
 def main():
-    print("Coral")
+    cluster_info = get_cluster_info()
+    pprint(cluster_info)
+
+def get_cluster_info():
+    # Connect to the cluster
+    cluster = rados.Rados(conffile="/etc/ceph/ceph.conf")
+    cluster.connect()
+
+    # Gather the data we need
+    cluster_info = {}
+    cluster_info['osds'] = get_osd_info(cluster)
+    cluster_info['pgs'] = get_pg_info(cluster)
+    cluster_info['pools'] = get_pool_info(cluster)
+    cluster_info['crush_rules'] = get_crush_rules(cluster)
+
+    # Disconnect from the cluster
+    cluster.shutdown()
+
+    # Return the cluster information
+    return cluster_info
+
+def get_osd_info(cluster):
+    osd_info = {}
+
+    # Get the up/in status from 'osd dump'
+    cmd = {'prefix': 'osd dump', 'format': 'json'}
+    ret, output, errs = cluster.mon_command(json.dumps(cmd), b'', timeout=5)
+    osd_dump = json.loads(output.decode('utf-8'))
+
+    for osd in osd_dump['osds']:
+        osd_id = osd['osd']
+        osd_info[osd_id] = {
+            'up': osd['up'],
+            'in': osd['in']
+        }
+
+    # Get the CRUSH weight, size, and device class from 'osd df'
+    cmd = {'prefix': 'osd df', 'format': 'json'}
+    ret, output, errs = cluster.mon_command(json.dumps(cmd), b'', timeout=5)
+    osd_df = json.loads(output.decode('utf-8'))
+
+    for osd in osd_df['nodes']:
+        osd_info[osd['id']]['crush_weight'] = osd['crush_weight']
+        osd_info[osd['id']]['size'] = osd['kb'] * 1024
+        osd_info[osd['id']]['device_class'] = osd['device_class']
+
+    return osd_info
+
+def get_pg_info(cluster):
+    # Grab the up/acting sets, state, and size for each PG
+    cmd = {'prefix': 'pg dump', 'format': 'json'}
+    ret, output, errs = cluster.mon_command(json.dumps(cmd), b'', timeout=5)
+    pg_dump = json.loads(output.decode('utf-8'))
+
+    pg_info = {}
+    for pg in pg_dump['pg_map']['pg_stats']:
+        pg_info[pg['pgid']] = {
+            'up': pg['up'].copy(),
+            'acting': pg['acting'].copy(),
+            'state': pg['state'],
+            'num_bytes': pg['stat_sum']['num_bytes'],
+            'upmaps': []
+        }
+
+    # Grab the existing upmaps
+    cmd = {'prefix': 'osd dump', 'format': 'json'}
+    ret, output, errs = cluster.mon_command(json.dumps(cmd), b'', timeout=5)
+    osd_dump = json.loads(output.decode('utf-8'))
+
+    # Convert the upmaps into a dictionary for easier access
+    upmaps = {}
+    for upmap in osd_dump['pg_upmap_items']:
+        pg_info[upmap['pgid']]['upmaps'] = [(mapping['from'], mapping['to']) for mapping in upmap['mappings']]
+
+    return pg_info
+
+def get_pool_info(cluster):
+    POOL_TYPE_NAMES = {1: 'replica', 3: 'erasure'}
+
+    # Grab name, type (replica/erasure) pg count, crush rule, and size (replica count/erase coding shards) for each pool
+    cmd = {'prefix': 'osd pool ls', 'detail': 'detail', 'format': 'json'}
+    ret, output, errs = cluster.mon_command(json.dumps(cmd), b'', timeout=5)
+    pool_info_full = json.loads(output.decode('utf-8'))
+
+    pool_info = {}
+    for pool in pool_info_full:
+        pool_info[pool['pool_id']] = {
+            'name': pool['pool_name'],
+            'type': POOL_TYPE_NAMES[pool['type']],
+            'crush_rule': pool['crush_rule'],
+            'pgs': pool['pg_num'],
+            'size': pool['size']
+        }
+
+    return pool_info
+
+def get_crush_rules(cluster):
+    # Grab all the CRUSH rules
+    cmd = {'prefix': 'osd crush rule dump', 'format': 'json'}
+    ret, output, errs = cluster.mon_command(json.dumps(cmd), b'', timeout=5)
+    crush_rule_dump = json.loads(output.decode('utf-8'))
+
+    crush_rules = {}
+    for rule in crush_rule_dump:
+        crush_rules[rule['rule_id']] = {
+            'name': rule['rule_name'],
+            'steps': rule['steps']
+        }
+
+    return crush_rules
 
 if __name__ == '__main__':
     main()
